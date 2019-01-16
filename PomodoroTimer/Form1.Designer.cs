87e1c236c76c714f36b3391416202cb0eba465ed
Michael Lauritsen <i30288741@live.dk>
2019-01-16 15:07:41 +0100

    added a function to start the timer and to display it correctly

--- PomodoroTimer/Form1.Designer.cs
+++ PomodoroTimer/Form1.Designer.cs
@@ -29,12 +29,94 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.TimerCount = new System.Windows.Forms.Label();
+            this.pauseCount = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.startBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TimerCount
+            // 
+            this.TimerCount.AutoSize = true;
+            this.TimerCount.Font = new System.Drawing.Font("Century Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimerCount.ForeColor = System.Drawing.Color.White;
+            this.TimerCount.Location = new System.Drawing.Point(38, 47);
+            this.TimerCount.Name = "TimerCount";
+            this.TimerCount.Size = new System.Drawing.Size(287, 115);
+            this.TimerCount.TabIndex = 0;
+            this.TimerCount.Text = "00:00";
+            // 
+            // pauseCount
+            // 
+            this.pauseCount.AutoSize = true;
+            this.pauseCount.Font = new System.Drawing.Font("Century Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pauseCount.ForeColor = System.Drawing.Color.White;
+            this.pauseCount.Location = new System.Drawing.Point(86, 162);
+            this.pauseCount.Name = "pauseCount";
+            this.pauseCount.Size = new System.Drawing.Size(191, 78);
+            this.pauseCount.TabIndex = 1;
+            this.pauseCount.Text = "00:00";
+            // 
+            // startBtn
+            // 
+            this.startBtn.AutoSize = true;
+            this.startBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(36)))), ((int)(((byte)(36)))));
+            this.startBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.startBtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.startBtn.FlatAppearance.BorderSize = 0;
+            this.startBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startBtn.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startBtn.ForeColor = System.Drawing.Color.Coral;
+            this.startBtn.Location = new System.Drawing.Point(67, 254);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(104, 36);
+            this.startBtn.TabIndex = 2;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = false;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.AutoSize = true;
+            this.resetBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(36)))), ((int)(((byte)(36)))));
+            this.resetBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.resetBtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.resetBtn.FlatAppearance.BorderSize = 0;
+            this.resetBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.resetBtn.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetBtn.ForeColor = System.Drawing.Color.Coral;
+            this.resetBtn.Location = new System.Drawing.Point(187, 254);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(104, 36);
+            this.resetBtn.TabIndex = 3;
+            this.resetBtn.Text = "Reset";
+            this.resetBtn.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(62)))), ((int)(((byte)(62)))));
+            this.ClientSize = new System.Drawing.Size(360, 450);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.pauseCount);
+            this.Controls.Add(this.TimerCount);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label TimerCount;
+        private System.Windows.Forms.Label pauseCount;
+        public System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button resetBtn;
     }
 }
 
--- PomodoroTimer/Form1.cs
+++ PomodoroTimer/Form1.cs
@@ -12,9 +12,33 @@ namespace PomodoroTimer
 {
     public partial class Form1 : Form
     {
+
+        DateTime endoftime = new DateTime();
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void t_Tick(object p1, object p2)
+        {
+            TimeSpan ts = endoftime.Subtract(DateTime.Now);            
+            TimerCount.Text = ts.ToString(@"mm\:ss");
+        }
+
+        public void startBtn_Click(object sender, EventArgs e)
+        {
+            startTimer();
+            
+        }
+
+        public void startTimer()
+        {
+            endoftime = DateTime.Now.AddMinutes(25);
+            Timer t = new Timer() { Interval = 1, Enabled = true };
+            t.Tick += new EventHandler(t_Tick);
+            t_Tick(null, null);
         }
     }
 }
