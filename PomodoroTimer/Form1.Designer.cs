4f375d215ba45c7f37935f5f2a737ebd68435891
Michael Lauritsen <i30288741@live.dk>
2019-01-18 13:56:12 +0100

    "done"

--- PomodoroTimer/Form1.Designer.cs
+++ PomodoroTimer/Form1.Designer.cs
@@ -30,34 +30,24 @@
         {
             this.components = new System.ComponentModel.Container();
             this.TimerCount = new System.Windows.Forms.Label();
-            this.pauseCount = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.startBtn = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.closeBtn = new System.Windows.Forms.Button();
+            this.TextLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // TimerCount
             // 
+            this.TimerCount.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.TimerCount.Font = new System.Drawing.Font("Century Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TimerCount.ForeColor = System.Drawing.Color.White;
-            this.TimerCount.Location = new System.Drawing.Point(38, 136);
+            this.TimerCount.Location = new System.Drawing.Point(37, 152);
             this.TimerCount.Name = "TimerCount";
             this.TimerCount.Size = new System.Drawing.Size(287, 115);
             this.TimerCount.TabIndex = 0;
             this.TimerCount.Text = "25:00";
-            this.TimerCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // pauseCount
-            // 
-            this.pauseCount.AutoSize = true;
-            this.pauseCount.Font = new System.Drawing.Font("Century Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.pauseCount.ForeColor = System.Drawing.Color.White;
-            this.pauseCount.Location = new System.Drawing.Point(86, 251);
-            this.pauseCount.Name = "pauseCount";
-            this.pauseCount.Size = new System.Drawing.Size(191, 78);
-            this.pauseCount.TabIndex = 1;
-            this.pauseCount.Text = "05:00";
+            this.TimerCount.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // startBtn
             // 
@@ -69,7 +59,7 @@
             this.startBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.startBtn.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.startBtn.ForeColor = System.Drawing.Color.Coral;
-            this.startBtn.Location = new System.Drawing.Point(64, 339);
+            this.startBtn.Location = new System.Drawing.Point(63, 339);
             this.startBtn.Name = "startBtn";
             this.startBtn.Size = new System.Drawing.Size(234, 36);
             this.startBtn.TabIndex = 2;
@@ -98,7 +88,7 @@
             this.closeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.closeBtn.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.closeBtn.ForeColor = System.Drawing.Color.Coral;
-            this.closeBtn.Location = new System.Drawing.Point(64, 381);
+            this.closeBtn.Location = new System.Drawing.Point(63, 381);
             this.closeBtn.Name = "closeBtn";
             this.closeBtn.Size = new System.Drawing.Size(234, 36);
             this.closeBtn.TabIndex = 4;
@@ -106,21 +96,35 @@
             this.closeBtn.UseVisualStyleBackColor = false;
             this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
             // 
+            // TextLabel
+            // 
+            this.TextLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.TextLabel.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextLabel.ForeColor = System.Drawing.Color.White;
+            this.TextLabel.Location = new System.Drawing.Point(57, 248);
+            this.TextLabel.Name = "TextLabel";
+            this.TextLabel.Size = new System.Drawing.Size(243, 69);
+            this.TextLabel.TabIndex = 5;
+            this.TextLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(62)))), ((int)(((byte)(62)))));
             this.ClientSize = new System.Drawing.Size(360, 450);
+            this.Controls.Add(this.TextLabel);
             this.Controls.Add(this.closeBtn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.startBtn);
-            this.Controls.Add(this.pauseCount);
             this.Controls.Add(this.TimerCount);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             this.TopMost = true;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -129,11 +133,11 @@
         #endregion
 
         private System.Windows.Forms.Label TimerCount;
-        private System.Windows.Forms.Label pauseCount;
         public System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button startBtn;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.Label TextLabel;
     }
 }
 
--- PomodoroTimer/Form1.cs
+++ PomodoroTimer/Form1.cs
@@ -21,6 +21,22 @@ namespace PomodoroTimer
             InitializeComponent();
             timer1.Interval = 100;
             timer1.Tick += timer1_Tick;
+
+            pausestart();
+
+        }
+        
+        private void pausestart()
+        {
+            if (TimerCount.Text == "0:00")
+            {
+                TextLabel.Text = "Break!";
+            }
+            else if (TimerCount.Text != "0:00")
+            {
+                TextLabel.Text = "";
+
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -34,8 +50,10 @@ namespace PomodoroTimer
 
             }
             TimerCount.Text = string.Format("{0}:{1:00}", sp.Minutes, sp.Seconds);
-        }
+            pausestart();
 
+        }
+        
         private void startBtn_Click(object sender, EventArgs e)
         {
             if (mPaused)
@@ -45,7 +63,7 @@ namespace PomodoroTimer
                 mTrigger = DateTime.Now + mPauseToGo;
                 startBtn.Text = "Pause";
             }
-            else if (timer1.Enabled)            
+            else if (timer1.Enabled)
             {
                 timer1.Enabled = false;
                 startBtn.Text = "Resume";
@@ -59,11 +77,37 @@ namespace PomodoroTimer
                 mTrigger = DateTime.Now + new TimeSpan(0, 25, 0);
                 timer1_Tick(this, EventArgs.Empty);
             }
+
         }
 
         private void closeBtn_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private bool mouseDown;
+        private Point lastLocation;
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                Location = new Point(
+                    (Location.X - lastLocation.X) + e.X, Location.Y - lastLocation.Y + e.Y);
+
+                Update();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
         }
     }
 }
